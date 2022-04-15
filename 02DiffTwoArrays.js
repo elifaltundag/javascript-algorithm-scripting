f634cb5164b4ee727cd098a032a366f080f8ff07
elifaltundag <elif.altundag.x@gmail.com>
2022-04-15 13:37:20 +0300

    Add source link

--- 02DiffTwoArrays.js
+++ 02DiffTwoArrays.js
@@ -1,5 +1,7 @@
 /* 
 Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays. 
+
+source: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/diff-two-arrays
 */
 
 
